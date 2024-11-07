bf2a939e487aa93b53b5ca77d22f9acd19a034ae
AdrianC2095 <adriancuenca095@gmail.com>
2024-11-07 12:36:08 -0500

    Ejercicio 5

--- Proyecto_Taller5/src/Ejercicio5_AñoBisiesto.java
+++ Proyecto_Taller5/src/Ejercicio5_AñoBisiesto.java
@@ -1,12 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
- *
- * @author Juan Jose Calva
+ * Año Bisiesto: Comprobar si un año ingresado es bisiesto o no.
+ * Si es bisiesto, mostrar un mensaje especial.
+ * @author Adrian Cuenca
  */
+import java.util.Scanner;
 public class Ejercicio5_AñoBisiesto {
+    public static void main(String[] args) {
+        int año;
+        Scanner tcl = new Scanner(System.in);
+        System.out.print("INGRESE UN AÑO: ");
+        año = tcl.nextInt();
+        if ((año % 4 == 0 && año % 100 != 0) || (año % 400 == 0))
+                System.out.println("!EL AÑO INGRESADO ES BICIESTO!");
+                else System.out.println("EL AÑO NO ES BICIESTO");                       
+    }
     
 }
