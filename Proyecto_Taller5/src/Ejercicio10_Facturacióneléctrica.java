87d3c7e72dfe288cbf946705808791c97323c01a
CalvitaJuanJo <juanjosecalva427@gmail.com>
2024-11-10 12:38:44 -0500

    Ejercicios Finalizados del 1 al 13

--- Proyecto_Taller5/src/Ejercicio10_Facturacióneléctrica.java
+++ Proyecto_Taller5/src/Ejercicio10_Facturacióneléctrica.java
@@ -1,12 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
+import java.util.Scanner;
+
+/***
+ * Facturación eléctrica: Desarrollar una solución que permita calcular y 
+ * mostrar el valor a cancelar de una planilla de luz. Se debe ingresar el 
+ * valor de costo por kilovatio/hora y el número de kilovatios consumidos en el
+ * mes. Si el usuario tiene edad mayor a 65 años, se debe descontar el 10% por 
+ * pertenecer a la tercera edad.
  * @author Juan Jose Calva
  */
 public class Ejercicio10_Facturacióneléctrica {
-    
-}
+    public static void main(String[] args) {
+        Scanner tcl = new Scanner(System.in);
+        double costoxkilovatio, kiloconsumido, valortotal;
+        int edad;
+        System.out.print("Ingrese el costo de kilovatio/hora:  ");
+        costoxkilovatio = tcl.nextDouble();
+        System.out.print("Ingrese el numero de kilovatios consumidos en el mes: ");
+        kiloconsumido = tcl.nextDouble(); 
+        System.out.print("Ingrese su edad: ");
+        edad = tcl.nextInt();
+        valortotal = costoxkilovatio * kiloconsumido;
+        if (edad > 65) {
+            valortotal *=0.9;
+        }
+        System.out.println("El valor de la planilla es: $" + valortotal);
+        }
+    }
+/***
+ * Ingrese el costo de kilovatio/hora:  10
+ * Ingrese el numero de kilovatios consumidos en el mes: 20
+ * Ingrese su edad: 66
+ * El valor de la planilla es: $180.0
+ */
--- Proyecto_Taller5/src/Ejercicio13_Sistemahoteleras.java
+++ Proyecto_Taller5/src/Ejercicio13_Sistemahoteleras.java
@@ -1,12 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
+import java.util.Scanner;
+
+/***
+ * Sistema de reservas hoteleras: En una hosteria de la ciudad de Loja se hace
+ * un descuento del 10% si el cliente se hospeda más de 5 días, del 15% si se 
+ * hospeda más de 10 días y del 20% si se hospeda más de 15 días. Elaborar un 
+ * solución que pida como datos de entrada el número de días y el precio diario
+ * de la habitación y luego calcule e imprima el subtotal por pagar, el 
+ * descuento y el total por pagar.
  * @author Juan Jose Calva
  */
 public class Ejercicio13_Sistemahoteleras {
-    
-}
+    public static void main(String[] args) {
+        Scanner tcl = new Scanner(System.in);
+        int dias;
+        double preciodiario, subtotal, descuento = 0, total;
+        
+        System.out.print("Ingrese el numero de dias de hospedaje; ");
+        dias = tcl.nextInt();
+        System.out.print("Ingrese el precio diario de la habitacion: ");
+        preciodiario = tcl.nextDouble();
+        
+        subtotal = dias * preciodiario;
+        if (dias > 15) {
+            descuento = subtotal * 0.2; 
+        } else if (dias > 10) {
+            descuento = subtotal * 0.15;
+        } else if (dias > 5) {
+            descuento = subtotal * 0.1;
+        }
+           total = subtotal - descuento;
+           
+            System.out.println("Subtotal por pagar: $" + subtotal);
+            System.out.println("Descuento aplicado: $" + descuento);
+            System.out.println("Total x pagar: $" + total);
+    }
+        
+    }
+/***
+ * Ingrese el numero de dias de hospedaje; 20
+ * Ingrese el precio diario de la habitacion: 100
+ * Subtotal por pagar: $2000.0
+ * Descuento aplicado: $400.0
+ * Total x pagar: $1600.0
+ */
